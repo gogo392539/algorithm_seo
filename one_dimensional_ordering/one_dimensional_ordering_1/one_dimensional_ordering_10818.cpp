1e3904a2473c618b62e5e09e6a36124498baf437
정오 서 <gogo392525@naver.com>
2020-11-26 08:54:17 +0900

    백준 10818, 2562 문제 추가

---
+++ one_dimensional_ordering/one_dimensional_ordering_1/one_dimensional_ordering_10818.cpp
@@ -0,0 +1,36 @@
+﻿// one_dimensional_ordering_1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+//백준 10818번 문제
+int main()
+{
+	int nNum = 0;
+	int nInputCount = 0;
+	vector<int> vData;
+	vector<int>::iterator vIterator;
+
+	cin >> nInputCount;
+
+	vData.reserve(nInputCount);
+
+	for (int i = 0; i < nInputCount; i++) {
+		cin >> nNum;
+
+		vData.push_back(nNum);
+	}
+
+	sort(vData.begin(), vData.end());
+
+	cout << vData.front() << " " << vData.back() << endl;
+
+
+
+	return 0;
+}
---
+++ one_dimensional_ordering/one_dimensioonal_ordering_2/one_dimensional_ordering_2562.cpp
@@ -0,0 +1,38 @@
+﻿// one_dimensioonal_ordering_2.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+
+using namespace std;
+
+//백준 2562번 문제
+int main()
+{
+	int narData[9] = { 0, };
+	vector<int> vData;
+	vData.reserve(9);
+	
+
+	for (int i = 0; i < 9; i++) {
+		cin >> narData[i];
+		vData.push_back(narData[i]);
+	}
+
+	sort(vData.begin(), vData.end());
+
+	cout << vData.back() << endl;
+
+	for (int i = 0; i < 9; i++) {
+		if (vData.back() == narData[i]) {
+			cout << i + 1 << endl;
+			break;
+		}
+	}
+
+	return 0;
+}
+
