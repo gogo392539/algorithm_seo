9ef08d64a73cb996449fbbfb21ffa7a71b48ead9
정오 서 <gogo392525@naver.com>
2020-12-09 08:36:21 +0900

    백준 3052번 추가

---
+++ one_dimensional_ordering/one_dimensional_ordering_2577/one_dimensional_ordering_2577.cpp
@@ -0,0 +1,47 @@
+﻿// one_dimensional_ordering_2577.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+//백준 2577번 문제
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int nData1 = 0;
+	int nData2 = 0;
+	int nData3 = 0;
+	int nMulData = 0;
+	string strMulData;
+
+	strMulData.clear();
+
+	cin >> nData1;
+	cin >> nData2;
+	cin >> nData3;
+
+	nMulData = nData1 * nData2 * nData3;
+
+	strMulData = to_string(nMulData);
+
+	for (int k = 0; k < 10; k++) {
+		int nCount = 0;
+		for (int i = 0; i < strMulData.size(); i++) {
+			int nTmpData = strMulData.at(i) - '0';
+			if (nTmpData == k) {
+				nCount++;
+			}
+		}
+		cout << nCount << "\n";
+	}
+
+
+	return 0;
+}
---
+++ one_dimensional_ordering/one_dimensional_ordering_3052/one_dimensional_ordering_3052.cpp
@@ -0,0 +1,37 @@
+﻿// one_dimensional_ordering_3052.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie();
+	cout.tie();
+
+	int nCount = 0;
+	int narData[10] = { 0, };
+	int narData2[10] = { 0, };
+	int narData3[42] = { 0, };
+
+	for (int i = 0; i < 10; i++) {
+		cin >> narData[i];
+	}
+
+	for (int i = 0; i < 10; i++) {
+		narData3[(narData[i] % 42)] = 1;
+	}
+
+	for (int i = 0; i < 42; i++) {
+		if (narData3[i] == 1) {
+			nCount++;
+		}
+	}
+
+	cout << nCount << "\n";
+
+	return 0;
+}
