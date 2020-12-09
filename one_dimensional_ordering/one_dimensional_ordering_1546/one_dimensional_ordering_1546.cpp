ea81d8002fc6b644ba550871bd679ce7acf7a59b
정오 서 <gogo392525@naver.com>
2020-12-09 09:52:42 +0900

    백준 1546, 8958번 추가

---
+++ one_dimensional_ordering/one_dimensional_ordering_1546/one_dimensional_ordering_1546.cpp
@@ -0,0 +1,44 @@
+﻿// one_dimensional_ordering_1546.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie();
+	cout.tie();
+
+	int nCount = 0;
+	double dMaxScore = 0;
+	double dAverage = 0;
+	vector<double> vScore;
+
+	cin >> nCount;
+
+	vScore.reserve(nCount);
+
+	for (int i = 0; i < nCount; i++) {
+		double dScore = 0;
+		cin >> dScore;
+		vScore.push_back(dScore);
+	}
+
+	dMaxScore = *max_element(vScore.begin(), vScore.end());
+
+	for (int i = 0; i < nCount; i++) {
+		double dTmpData = 0.0;
+		dTmpData = (double)(vScore[i] / dMaxScore * 100.0);
+
+		dAverage += dTmpData;
+	}
+
+	cout << dAverage / (double)nCount << "\n";
+
+	return 0;
+}
---
+++ one_dimensional_ordering/one_dimensional_ordering_8958/one_dimensional_ordering_8958.cpp
@@ -0,0 +1,72 @@
+﻿// one_dimensional_ordering_8958.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+/*등차 수열의 합을 구하는 함수*/
+int fn_IsometricSequenceSum(int nFirstTerm/*첫째항*/, int nCommonDifference /*공차*/, int nCountOfTerm /*항의 개수*/);
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie();
+	cout.tie();
+
+	int nCount = 0;
+	
+	cin >> nCount;
+	
+	for (int j = 0; j < nCount; j++) {
+		int nSum = 0;
+		int nCorrectCount = 0;
+		char arQuizResult[80] = { 0, };
+
+		cin >> arQuizResult;
+
+		for (int i = 0; i < strlen(arQuizResult); i++) {
+			if (arQuizResult[i] == 'O') {
+				nCorrectCount++;
+			}
+
+			if (arQuizResult[i] == 'X') {
+				nSum += fn_IsometricSequenceSum(1, 1, nCorrectCount);
+				nCorrectCount = 0;
+			}
+		}
+
+		if (nCorrectCount != 0) {
+			nSum += fn_IsometricSequenceSum(1, 1, nCorrectCount);
+		}
+
+		cout << nSum << "\n";
+	}
+
+	
+
+
+	return 0;
+}
+
+/*등차 수열의 합을 구하는 함수*/
+int fn_IsometricSequenceSum(int nFirstTerm, int nCommonDifference, int nCountOfTerm)
+{
+	int nResult = 0;
+	int nEndTerm = 0;
+
+	if (nCommonDifference == 0) {
+		return 0;
+	}
+
+	if (nCountOfTerm == 1) {
+		return 1;
+	}
+
+	nEndTerm = nFirstTerm + ((nCountOfTerm - 1) * nCommonDifference);
+
+	nResult = nCountOfTerm * (nFirstTerm + nEndTerm) / 2;
+
+	return nResult;
+}
