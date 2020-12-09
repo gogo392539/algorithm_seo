5db6d36e49d00d9f0e2104364193a0ac7fb82f98
정오 서 <gogo392525@naver.com>
2020-12-09 11:55:01 +0900

    백준 15596 추가

---
+++ function/function_15596/function_15596.cpp
@@ -0,0 +1,39 @@
+﻿// function_15596.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+long long sum(std::vector<int>& a);
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie();
+	cout.tie();
+
+
+
+
+	return 0;
+}
+
+long long sum(std::vector<int>& a) {
+	long long ans = 0;
+
+	/*accumulate()를 이용한 합 구하는 방법*/
+	ans = accumulate(a.begin(), a.end(), 0LL);
+
+	/*
+	//vector 데이터를 직접 다 더하는 방법
+	for (int i = 0; i < a.size(); i++) {
+		ans += a[i];
+	}
+	*/
+
+	return ans;
+}
---
+++ function/function_4673/function_4673.cpp
@@ -0,0 +1,14 @@
+﻿// function_4673.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+	return 0;
+}
