c77b59f66cb2ad4b5325eeeacd998d7ecb9564b0
정오 서 <gogo392525@naver.com>
2021-01-11 09:12:12 +0900

    프로그래머스 문제 시작

--- function/function_4673/function_4673.cpp
+++ function/function_4673/function_4673.cpp
@@ -3,12 +3,40 @@
 
 #include "pch.h"
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+void fn_Check_SelfNumber(vector<int>& vData);
+
 int main()
 {
+	vector<int> vData;
+	vData.reserve(10000);
 
+	for (int i = 0; i < 10000; i++) {
+		vData.push_back(i + 1);
+	}
+
+	for (int i = 0; i < 10000; i++) {
+		fn_Check_SelfNumber(vData);
+	}
+
+	for (int i = 0; i < 10000; i++) {
+		if (vData[i] != 0) {
+			cout << vData[i] << "\n";
+		}
+	}
+
+
+	
 
 	return 0;
+}
+
+void fn_Check_SelfNumber(vector<int>& vData)
+{
+	
+
+
 }
