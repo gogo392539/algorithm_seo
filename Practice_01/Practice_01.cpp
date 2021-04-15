c1753794da1038123c6cb46f648905e1aec0f7bc
정오 서 <gogo392525@naver.com>
2021-04-15 10:39:24 +0900

    알고리즘 강의

---
+++ Practice_01/Practice_01.cpp
@@ -0,0 +1,46 @@
+﻿// Practice_01.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+void fn_Remainder(); //10430
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	fn_Remainder();
+
+}
+
+
+//10430번
+void fn_Remainder() {
+	int nA = 0;
+	int nB = 0;
+	int nC = 0;
+	int nResult1 = 0;
+	int nResult2 = 0;
+	int nResult3 = 0;
+	int nResult4 = 0;
+
+	cin >> nA >> nB >> nC;
+
+	nResult1 = (nA + nB) % nC;
+	nResult2 = ((nA % nC) + (nB % nC)) % nC;
+	nResult3 = (nA * nB) % nC;
+	nResult4 = ((nA & nC) * (nB % nC)) % nC;
+
+
+	cout << nResult1 << "\n";
+	cout << nResult2 << "\n";
+	cout << nResult3 << "\n";
+	cout << nResult4 << "\n";
+}
+
+
--- programmers/programmers4/programmers4.cpp
+++ programmers/programmers4/programmers4.cpp
@@ -1,6 +1,8 @@
 ﻿// programmers4.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
+// 문제 : 주식가격(LEVEL 2)
+
 #include "pch.h"
 #include <iostream>
 #include <vector>
@@ -10,12 +12,22 @@
 using namespace std;
 
 void printVector(vector<int> vData);
-
+vector<int> solution(vector<int> prices);
 
 int main()
 {
 
 	return 0;
+}
+
+vector<int> solution(vector<int> prices) 
+{
+	vector<int> answer;
+
+
+
+
+	return answer;
 }
 
 void printVector(vector<int> vData)
